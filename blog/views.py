d1116ddb3e51d25e8a82e02c30bf25171875895e
Ishan Pattnayak <ishanpattnayak@gmail.com>
2021-08-04 21:06:45 +0530

    Final changes

--- blog/views.py
+++ blog/views.py
@@ -4,12 +4,12 @@ from blog.models import Blogs
 
 def home(request):
     blogs = Blogs.objects.all()
-    print(blogs)
+    #print(blogs)
     params = {'blogs':blogs}
     return render(request , 'blog/bloghome.html', params)
 
 def blogPost(request,myid): #slug variable should be same.. as in urls.py
-    print(myid)
+    #print(myid)
     # return HttpResponse(f'Blog post working...{slug}') #An fstring for checking slug...
     blogPost = Blogs.objects.filter(sno=myid).first
     params = {'blogPost':blogPost}
